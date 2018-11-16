65ee6c2441407e936e9fb6ac63595105785ed7f3
Eleanor Jones <Eleanor.Jones.1@warwick.ac.uk>
2018-11-16 10:16:09 +0000

    Playfair Cipher with both encryption and decryption working and certain cases excluded.

--- MPAGSCipher/PlayfairCipher.cpp
+++ MPAGSCipher/PlayfairCipher.cpp
@@ -59,7 +59,6 @@ void PlayfairCipher::setkey ( const std::string& key )
   };
   auto iter2 = std::remove_if(key_.begin(), key_.end(), dup);
   key_.erase (iter2, key_.end());
-  std::cout <<key_<<std::endl;
 
   // store the coords of each letter
   // store the playfair cipher key map
@@ -70,14 +69,6 @@ void PlayfairCipher::setkey ( const std::string& key )
     letter_.insert(p0);
     letter_coords_.insert(p1);
   }
-  /* for (auto p : letter_)
-    {
-      std::cout << p.first.first << "," << p.first.second << " : " << p.second << std::endl;
-      }
-    for (auto q : letter_coords_)
-    {
-      std::cout << q.first << ": " << q.second.first << "," << q.second.second << std::endl;
-      } */
 }
 
 std::string PlayfairCipher::applyCipher( const std::string& inputText, const CipherMode cipherMode ) const
@@ -85,48 +76,62 @@ std::string PlayfairCipher::applyCipher( const std::string& inputText, const Cip
   std::string input = inputText;
   std::string output {""};
 
-  // change J->I
-  auto transform = [] (char b) {
-    if (b=='J'){
-      return 'I';
-    }
-    else {
-      return b;
-    }
-  };
-  std::transform(input.begin(), input.end(), input.begin(), transform);
+  // - check if the mode is encrypting AND the input word ends with a Z. If both are true, output a message saying the cipher cannot work with words ending in Z
+  if (cipherMode == CipherMode::Encrypt && input.back() == 'Z') {
+    std::cout << "Cipher cannot encrypt words which end in a Z" << std::endl;
+    std::cout << "Returning the user input" << std::endl;
+    output = input;
+  }
+  
+  // - check if the mode is decrypting AND the input word has an odd number of characters. If both are true, output a message saying the cipher cannot decrypt a word with an odd number of letters by definition. 
+  else if (cipherMode == CipherMode::Decrypt && input.size() % 2 == 1) {
+    std::cout << "Cipher cannot decrypt words with an odd number of letters" << std::endl;
+    std::cout<< "Returning the user input" << std::endl;
+    output = input;
+  }
 
-  // if repeated chars in a digraph add an X or Q if XX
-  for (size_t i{1}; i<input.size(); ++i) {
-    if (input[i]==input[i-1]){
-      if (input[i]=='X'){
-	input.insert(i,"Q");
+  else { 
+    // change J->I
+    auto transform = [] (char b) {
+      if (b=='J'){
+	return 'I';
       }
       else {
-	input.insert(i,"X");
+	return b;
+      }
+    };
+    std::transform(input.begin(), input.end(), input.begin(), transform);
+    
+    // if repeated chars in a digraph add an X or Q if XX
+    for (size_t i{1}; i<input.size(); ++i) {
+      if (input[i]==input[i-1]){
+	if (input[i]=='X'){
+	  input.insert(i,"Q");
+	}
+	else {
+	  input.insert(i,"X");
+	}
       }
     }
-  }
-
-  // if the size of input is odd, add a trailing Z
-  if ( input.size() % 2 == 1) {
-    input += 'Z';
-  }
-
-  std::cout << input << std::endl;
-
-  // loop over the input in Digraphs
-  for (auto initer{input.cbegin()}; initer != input.cend(); initer += 2) {
-
-  //   - find the coords in the grid for each digraph
+    
+    // if the size of input is odd, add a trailing Z
+    if ( input.size() % 2 == 1) {
+      input += 'Z';
+    }
+    
+    // loop over the input in Digraphs
+    for (auto initer{input.cbegin()}; initer != input.cend(); initer += 2) {
+      
+      //   - find the coords in the grid for each digraph
       switch (cipherMode) {
         case CipherMode::Encrypt : 
 	  {
 	    auto find = letter_coords_.find(*initer);
 	    auto find2 = letter_coords_.find(*(initer+1));
 	    std::pair<int,int> newcoords1 {0,0};
-    	    std::pair<int,int> newcoords2 {0,0};
-  //   - apply the rules to these coords to get 'new' coords
+	    std::pair<int,int> newcoords2 {0,0};
+	    
+	    //   - apply the rules to these coords to get 'new' coords
 	    if ((*find).second.first == (*find2).second.first) {
 	      newcoords1.first = (*find).second.first;
 	      newcoords1.second = ((*find).second.second + 1) % 5;
@@ -145,17 +150,63 @@ std::string PlayfairCipher::applyCipher( const std::string& inputText, const Cip
 	      newcoords2.first = (*find2).second.first;
 	      newcoords2.second = (*find).second.second;
 	    }
-
-  //   - find the letter associated with the new coords
+	    
+	    //   - find the letter associated with the new coords
 	    auto refind = letter_.find(newcoords1);
 	    auto refind2 = letter_.find(newcoords2);
 	    output += (*refind).second;
 	    output += (*refind2).second;
 	    break;
 	  }
+	  
+      case CipherMode::Decrypt :
+	{
+	  // check if there are double letters in the input, and if there are, output a message saying that the word to be decrypted should not contain double letters by definition
+	  
+	  // check if the size of input is odd, and if it is, output a message saying that the word to be decrypted should have an even number of characters by definition
+	  /* if ( input.size() % 2 == 1) {
+	     std::cout << "The word to be decrypted should have an even number of letters" << std::endl;
+	     output = input;
+	     }
+	     
+	     else { */
+	  
+	  //   - find the coords in the grid for each digraph
+	  auto find = letter_coords_.find(*initer);
+	  auto find2 = letter_coords_.find(*(initer+1));
+	  std::pair<int,int> newcoords1 {0,0};
+	  std::pair<int,int> newcoords2 {0,0};
+	  
+	  //   - apply the rules to these coords to get 'new' coords
+	  if ((*find).second.first == (*find2).second.first) {
+	    newcoords1.first = (*find).second.first;
+	    newcoords1.second = ((*find).second.second + 4) % 5;
+	    newcoords2.first = (*find).second.first;
+	    newcoords2.second = ((*find2).second.second + 4) % 5;
+	  }
+	  else if ((*find).second.second == (*find2).second.second) {
+	    newcoords1.first = ((*find).second.first + 4) % 5;
+	    newcoords1.second = (*find).second.second;
+	    newcoords2.first = ((*find2).second.first + 4) % 5;
+	    newcoords2.second = (*find).second.second;
+	  }
+	  else {
+	    newcoords1.first = (*find).second.first;
+	    newcoords1.second = (*find2).second.second;
+	    newcoords2.first = (*find2).second.first;
+	    newcoords2.second = (*find).second.second;
+	  }
+	  
+	  //   - find the letter associated with the new coords
+	  auto refind = letter_.find(newcoords1);
+	  auto refind2 = letter_.find(newcoords2);
+	  output += (*refind).second;
+	  output += (*refind2).second;
+	  break;
+	}
       }
+    }  
   }
-
   // return the text
 
   return output;
